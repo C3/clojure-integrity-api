383f21ef5d20ed6be0f8ef2c4c60afa92318c533
Beau Fabry <beau.fabry@c3businesssolutions.com>
2012-09-19 11:43:33 +1000

    first commit

---
+++ src/java_integrity_api/core.clj
@@ -0,0 +1,1 @@
+(ns java-integrity-api.core)
---
+++ test/java_integrity_api/test/core.clj
@@ -0,0 +1,6 @@
+(ns java-integrity-api.test.core
+  (:use [java-integrity-api.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
